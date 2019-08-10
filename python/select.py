3a0ae8f0846f0c7990a7ea63938fc78cfc2e7f75
xyzhang <13072287533@163.com>
2019-08-10 15:45:27 +0800

    aaaaa

--- python/select.py
+++ python/select.py
@@ -4,6 +4,8 @@ import re
 import sys 
 import os
 import codecs
+import codecs
+import argparse
 
 # _*_ coding:utf-8 _*_
 def select(num):
@@ -13,7 +15,7 @@ def select(num):
          lines = handle.readlines()
          new_file = open('bbb.txt','w')
          for line in lines:
-            if (conuter <= 6 or conuter >= 27):         #在python中，异或非分别用and or not 表示
+            if (conuter <= 6 or conuter >= 27):         
                print(conuter)
                top_line = re.sub(r'xxxxxx','ddd',line)
                new_file.write(top_line)
@@ -25,4 +27,10 @@ def select(num):
       pass
 
 if __name__ == '__main__':
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-mn','--modules_name', default = 0, help = 'this is shenme ')
+   parser.add_argument('-num','--num', default = 0, help = 'this is shenme ')
+   args = parser.parse_args()
+   modules = args.module_name;
+   num = args.num;
    select(0)
