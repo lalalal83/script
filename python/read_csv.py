9cd01e6c143a45327a01f4a94fa68bb7d623a528
xyzhang <13072287533@163.com>
2019-08-12 18:49:18 +0800

    read_csv

---
+++ python/read_csv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import re
+import sys 
+import os
+import codecs
+import pandas as pd
+
+def read_csv0():
+   de = pd.read_csv('test.csv')
+   print(de)
+
+if __name__ == '__main__'
+   read_csv0()
+
