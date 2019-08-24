90b333bd9823faed5770aa1b4a34548befdec713
xyzhang <13072287533@163.com>
2019-08-24 14:58:25 +0800

    2019.8.24

--- python/uvm_gen.py
+++ python/uvm_gen.py
@@ -23,19 +23,45 @@ class Uvm_gen():
       os.system(mkdir)
 
    def rename_file(self,module_name):
-      os.system('cp -r ../uvm_gen/* ./')
+      os.system('cp -r ../uvm_gen/templates/* ./' + module_name + '_verfi')
       path = os.getcwd()
       print(path)
       path = path + '/' + module_name + '_verfi'
       os.chdir(path)
       print(os.getcwd())
+      filelist = os.listdir(path)
+      for file in filelist:
+         '''rename this file'''
+         old_name = path + os.sep + file
+         new_file = re.sub(r'uvm',module_name,file)
+         new_name = path + os.sep + new_file
+         os.rename(old_name,new_name)
 
+   def new_content(self,module_name):
+      path = os.getcwd()
+      filelist =  os.listdir(path)
+      for file1 in filelist:
+         with open(file1) as handle:
+            handle_lines = handle.readlines()
+         with open(file1,'w') as new_handle:
+            for line in handle_lines:
+               if '{:NAME:}' in line:
+                  line = re.sub(r'{:NAME:}',module_name,line)
+               if '{:UPPERNAME:}' in line:
+                  line = re.sub(r'{:UPPERNAME:}',module_name.upper(),line)
+               if '' in line:
+                  #new_line = re
+                  pass
+               new_handle.write(line)
+
+      
 if __name__ == '__main__':
    os.system('rm -r ' + modules + '_verfi')
    print('clean old file %(modules_name)s_verfi' % {'modules_name' : modules})
    uvm_gen = Uvm_gen(modules,1)
    uvm_gen.mkdir_file(modules)
    uvm_gen.rename_file(modules)
+   uvm_gen.new_content(modules)
       
 
 
